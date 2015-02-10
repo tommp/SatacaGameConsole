b90e249ec86470a0310c050e91a8a16301ae1d69
morteano <morteano@stud.ntnu.no>
2015-02-10 13:59:35 +0100

    followed the instructions for timer.c

--- ex2/timer.c
+++ ex2/timer.c
@@ -6,16 +6,17 @@
 /* function to setup the timer */
 void setupTimer(uint16_t period)
 {
-  /*
-    TODO enable and set up the timer
-    
-    1. Enable clock to timer by setting bit 6 in CMU_HFPERCLKEN0
-    2. Write the period to register TIMER1_TOP
-    3. Enable timer interrupt generation by writing 1 to TIMER1_IEN
-    4. Start the timer by writing 1 to TIMER1_CMD
-    
-    This will cause a timer interrupt to be generated every (period) cycles. Remember to configure the NVIC as well, otherwise the interrupt handler will not be invoked.
-  */  
+  //Enable clock to timer by setting bit 6 in CMU_HFPERCLKEN0
+  *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_TIMER1;
+  
+  //Write the period to register TIMER1_TOP
+  *TIMER1_TOP = period;
+  
+  //Enable timer interrupt generation by writing 1 to TIMER1_IEN
+  *TIMER1_IEN |= TIMER1_IEN_INTR;
+  
+  //Start the timer by writing 1 to TIMER1_CMD
+  TIMER1_CMD |= TIMER1_CMD_START;
 }
 
 
