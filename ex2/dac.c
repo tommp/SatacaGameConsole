499e3103d77234f009a92534a18ba46491818259
morteano <morteano@stud.ntnu.no>
2015-02-10 13:39:57 +0100

    changed to the convension in the header file

--- ex2/dac.c
+++ ex2/dac.c
@@ -12,8 +12,8 @@ void setupDAC()
     *DAC0_CTRL = 0x050010;
     
     //Enable left and right audio channels by writing 1 to DAC0_CH0CTRL and DAC0_CH1CTRL
-    *DAC0_CH0CTRL |= (1 << DAC_CHnCTRL_EN);
-    *DAC0_CH1CTRL |= (1 << DAC_CHnCTRL_EN);
+    *DAC0_CH0CTRL |= DAC_CHnCTRL_EN;
+    *DAC0_CH1CTRL |= DAC_CHnCTRL_EN;
     
     //Write a continuous stream of samples to the DAC data registers, DAC0_CH0DATA and DAC0_CH1DATA, for example from a timer interrupt
     //TODO
