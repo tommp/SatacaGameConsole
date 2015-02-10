fb57861e7cb04c458200feb8733375d1fdec9806
morteano <morteano@stud.ntnu.no>
2015-02-10 14:01:44 +0100

    set SAMPLE_PERIOD to 400 for testing

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -10,7 +10,7 @@
   registers are 16 bits.
 */
 /* The period between sound samples, in clock cycles */
-#define   SAMPLE_PERIOD   0
+#define   SAMPLE_PERIOD   400
 
 /* Declaration of peripheral setup functions */
 void setupTimer(uint32_t period);
