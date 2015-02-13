3ab86d61d8161be01dc46ef5f01d406349f12927
Magne Alver Normann <magnealv@dmlab11.(none)>
2015-02-13 13:06:27 +0100

    added frequency in period calculation

--- ex2/timer.c
+++ ex2/timer.c
@@ -3,6 +3,8 @@
 
 #include "efm32gg.h"
 
+#define CLOCK_FREQUENCY 14000000
+
 /* function to setup the timer */
 void setupTimer(uint16_t period)
 {
@@ -10,7 +12,7 @@ void setupTimer(uint16_t period)
   *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_TIMER1;
   
   //Write the period to register TIMER1_TOP
-  *TIMER1_TOP = period;
+  *TIMER1_TOP = CLOCK_FREQUENCY/period;
   
   //Enable timer interrupt generation by writing 1 to TIMER1_IEN
   *TIMER1_IEN |= TIMER1_IEN_INT;
