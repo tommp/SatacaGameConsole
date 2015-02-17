057ddc7edb2f1ff4380a738ad0d9daf58ea06f22
tommp <tomsju@hotmail.com>
2015-02-17 16:11:47 +0100

    Added music_lib for playing tunes

---
+++ ex2/music_lib/tunes.h
@@ -0,0 +1,43 @@
+#define A_FREQUENCY 27
+#define B_FREQUENCY 30
+#define C_FREQUENCY	16
+#define D_FREQUENCY	18
+#define E_FREQUENCY	20
+#define F_FREQUENCY 22
+#define G_FREQUENCY 24
+
+//If set to 0, expects one-shot mode.
+#define USE_FREE_RUN 0
+
+//Set to one if using low energy timer
+#define USE_LET 0
+
+//Frequency of the timer clock
+#define TIMER_FREQUENCY 32678
+
+#define TOP_VALUE_REGISTER 0xlolfochizzle
+#define TIME_TICKS_REGISTER 0xchizzlemahnizzle
+
+//Linked list node for a tune
+typedef struct Tune{
+	//Pointer to the following tune node
+	Tune* next;
+	//The note of the tune
+	char note; 
+	//The octave of the tune
+	int octave;	
+	//Determines time to play the tune (in miliseconds)
+	int time_to_play_ms; 
+} Tune;
+
+//Linked list of tunes representing a song
+typedef struct Song{
+	//Pointer to the first note in the sequence
+	Tune* first;
+	//Pointer to the current note (the one to be played)
+	Tune* current;
+} Song;
+
+int play_tune(Tune* tune);
+int playCurrentAndSetNextTune(Song* song);
+int construct_song(char* notes, int* timers, Song* song, int length, int octave);
