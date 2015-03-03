c50a5f6b1e26364978888abf5ceadc06fb37c986
Morten Alver Normann <morteano@dmlab10.(none)>
2015-03-03 13:17:41 +0100

    rm moot func and comments

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -7,14 +7,8 @@
 #include "timer.h"
 #include "dac.h"
 
-/* 
-  TODO calculate the appropriate sample period for the sound wave(s) 
-  you want to generate. The core clock (which the timer clock is derived
-  from) runs at 14 MHz by default. Also remember that the timer counter
-  registers are 16 bits.
-*/
 /* The period between sound samples, in clock cycles */
-#define   SAMPLE_RATE   500
+#define   SAMPLE_RATE   5000
 #define   SAMPLE_RATE2  32768
 
 /* Declaration of peripheral setup functions */
@@ -25,74 +19,15 @@ int main(void)
 {  
   /* Call the peripheral setup functions */
   setupGPIO();
-  setupDAC();
+  //dac_init();
   //setupTimer(SAMPLE_RATE);
   setupLETimer(SAMPLE_RATE);
+ 
   
-  /* Enable interrupt handling */
-  //setupNVIC();
-  
-  /* TODO for higher energy efficiency, sleep while waiting for interrupts
-     instead of infinite loop for busy-waiting
-  */
+  *SCR = 6;
+  __asm("WFI");
 
   while(1);
 
   return 0;
 }
-
-/* if other interrupt handlers are needed, use the following names: 
-   NMI_Handler
-   HardFault_Handler
-   MemManage_Handler
-   BusFault_Handler
-   UsageFault_Handler
-   Reserved7_Handler
-   Reserved8_Handler
-   Reserved9_Handler
-   Reserved10_Handler
-   SVC_Handler
-   DebugMon_Handler
-   Reserved13_Handler
-   PendSV_Handler
-   SysTick_Handler
-   DMA_IRQHandler
-   GPIO_EVEN_IRQHandler
-   TIMER0_IRQHandler
-   USART0_RX_IRQHandler
-   USART0_TX_IRQHandler
-   USB_IRQHandler
-   ACMP0_IRQHandler
-   ADC0_IRQHandler
-   DAC0_IRQHandler
-   I2C0_IRQHandler
-   I2C1_IRQHandler
-   GPIO_ODD_IRQHandler
-   TIMER1_IRQHandler
-   TIMER2_IRQHandler
-   TIMER3_IRQHandler
-   USART1_RX_IRQHandler
-   USART1_TX_IRQHandler
-   LESENSE_IRQHandler
-   USART2_RX_IRQHandler
-   USART2_TX_IRQHandler
-   UART0_RX_IRQHandler
-   UART0_TX_IRQHandler
-   UART1_RX_IRQHandler
-   UART1_TX_IRQHandler
-   LEUART0_IRQHandler
-   LEUART1_IRQHandler
-   LETIMER0_IRQHandler
-   PCNT0_IRQHandler
-   PCNT1_IRQHandler
-   PCNT2_IRQHandler
-   RTC_IRQHandler
-   BURTC_IRQHandler
-   CMU_IRQHandler
-   VCMP_IRQHandler
-   LCD_IRQHandler
-   MSC_IRQHandler
-   AES_IRQHandler
-   EBI_IRQHandler
-   EMU_IRQHandler
-*/
