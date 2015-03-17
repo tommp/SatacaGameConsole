f3323b83fa88c9e438e7c6231c667dec465d7bd6
Magne Alver Normann <magnealv@dmlab10.(none)>
2015-03-17 13:34:11 +0100

    implemented driver init functions

--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -5,6 +5,11 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/kdev_t.h>   // used for dev_t macros that holds the major and minor number
+#include <linux/cdev.h>     // used for cdev structs (internal representation of char devices
+#include <linux/fs.h>       // used for struct file_operations struct
+#include <driver-gamepad.h>
+
 
 /*
  * template_init - function to insert this module into kernel space
@@ -15,9 +20,35 @@
  * Returns 0 if successfull, otherwise -1
  */
 
-static int __init template_init(void)
+static int __init driver_init(void)
 {
-	printk("Hello World, Tron is awesome\n");
+	/* Get major number for the device */
+    int err = alloc_chrdev_region(&dev_nr, 0, COUNT, DEVICE_NAME);
+    if(err < 0){
+        printk(KERN_WARNING "Error: Unable to allocate driver region. err = %d", err);
+    }else{
+        printk("Driver region allocated");
+    }
+    
+    /* init the struct for internal representation of the char driver */
+    cdev_init(&driver_cdev, &fops);
+    
+    driver_cdev.owner = THIS_MODULE;    // TODO:: Try removing this line, it might be obsolete.
+    
+    /* Register the char device with the kernel */
+    err = cdev_add(&driver_cdev, dev_nr, COUNT);
+    if(err < 0){
+        printk(KERN_WARNING "Error: Unable to register device with kernel. err = %d", err);
+    }else{
+        printk("Char device registered with Kernel");
+    }
+    
+    /* Enable the driver to appear in the /dev directory */
+    cl = class_create(THIS_MODULE, DEVICE_NAME);
+    device_create(cl, NULL, devno, NULL, DEVICE_NAME);
+    
+    
+    /* Init the char device struct */	
 	return 0;
 }
 
@@ -31,6 +62,14 @@ static int __init template_init(void)
 static void __exit template_cleanup(void)
 {
 	 printk("Short life for a small module...\n");
+	 
+	 /* Remove the char device */
+	 err = cdev_del(&driver_cdev);
+    if(err < 0){
+        printk(KERN_WARNING "Error: Unable to register device with kernel. err = %d", err);
+    }else{
+        printk("Char device registered with Kernel");
+    }
 }
 
 module_init(template_init);
