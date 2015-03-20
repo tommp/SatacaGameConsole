c1522460d0b47ccddebdf5d71feb4e6ba4e430ea
Magne Alver Normann <magnealv@dmlab11.(none)>
2015-03-20 12:55:11 +0100

    implemented opening of framebuffer file

--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/display.c
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/display.c
@@ -1,6 +1,16 @@
 #include <linux/fb.h>   // used for the framebuffer
+#include <sys/stat.h>   // used for open()
+#include <fcntl.h>      // used for open()
 
 #include "display.h"
 
 
+int display_init(){
+    fd = open("/dev/fb0", O_RDWR);
+    if(fd == -1){
+           
+    }
+    
+    
+}
 
--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/display.h
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/display.h
@@ -1,8 +1,14 @@
 #ifndef _DISPLAY_API_H
 #define _DISPLAY_API_H
 
+#define DISPLAY_HEIGHT 240
+#define DISPLAY_WIDTH 320
+
 /* Struct to hold the rectangle of the framebuffer that is to be updated */
 struct fb_copyarea rect;
+
+/* file descriptor for det display buffer */
+int fd;
 
 
 
