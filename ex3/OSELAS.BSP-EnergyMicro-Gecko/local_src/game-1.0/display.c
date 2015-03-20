288601d324920dbbb0905949f85d97cd0062dd3b
Magne Alver Normann <magnealv@dmlab11.(none)>
2015-03-20 13:58:07 +0100

    finished display_init funtion

--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/display.c
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/display.c
@@ -1,16 +1,44 @@
 #include <linux/fb.h>   // used for the framebuffer
 #include <sys/stat.h>   // used for open()
 #include <fcntl.h>      // used for open()
+#include <sys/mman.h>   // used for mmap()
 
 #include "display.h"
 
 
-int display_init(){
-    fd = open("/dev/fb0", O_RDWR);
-    if(fd == -1){
-           
+int display_init(void){
+
+    /* Open file reperesenting the display */
+    fbfd = open("/dev/fb0", O_RDWR);
+    if(fbfd == ERROR){
+        printf("Error: Failed to open file\n");
+        //TODO:: add exit/errno action
+    }else{
+        printf("Display file opened succesfully\n");
     }
     
+    /* Get screen info from framebuffer driver 
+    if(ioctl(fd, FBIOGET_VSCREENINFO, &screen_info) == ERROR){
+        printf("Error: Failed to get screen info\n");
+        //TODO:: add exit/errno action
+    }else{
+        printf("Screeninfo received succesfully\n");
+    }
+    */
+    
+    /* Map the screen pixel values directly to memory */
+    screen_pxl_vals =(uint16_t*) mmap(
+            NULL, 
+            SCREEN_SIZE_BYTES, 
+            PROT_READ |
+            PROT_WRITE, 
+            MAP_SHARED, 
+            fp, 
+            0); 
+    if (screen_pxl_vals == MAP_FAILED){     //MAP_FAILED = ((void*) -1)
+        printf("Error: Failed mapping screen to memory\n");
+        //TODO:: add exit/errno action      
+    }
     
 }
 
--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/display.h
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/display.h
@@ -1,16 +1,30 @@
-#ifndef _DISPLAY_API_H
-#define _DISPLAY_API_H
+#ifndef _DISPLAY_H
+#define _DISPLAY_H
 
-#define DISPLAY_HEIGHT 240
-#define DISPLAY_WIDTH 320
+#define SCREEN_HEIGHT   240
+#define SCREEN_WIDTH    320
+#define BITS_PER_PIXEL  16
+#define SCREEN_SIZE_BYTES (SCREEN_HEIGHT*SCREEN_WIDTH*BITS_PER_PIXEL/8)
+#define N_BITS_RED      5
+#define N_BITS_GREEN    5
+#define N_BITS_BLUE     6
+
+#define ERROR   (-1)
 
 /* Struct to hold the rectangle of the framebuffer that is to be updated */
 struct fb_copyarea rect;
 
 /* file descriptor for det display buffer */
-int fd;
+int fbfd;
+
+/* struct fb_var_screeninfo stores device independent changeable information
+about a frame buffer device, its current format and video mode, as well as
+other miscellaneous parameters */
+struct fb_var_screeninfo screen_info;
 
 
+uint16_t *screen_pxl_vals;
 
+int display_init(void);
 
 #endif // _DISPLAY_API_H
