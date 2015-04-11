b497eaae62131973edb62c6f368c9cdbff74957f
Morten Alver Normann <morteano@dmlab09.(none)>
2015-04-11 15:26:22 +0200

    implemented gameengine, needs to be debugged

--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -7,17 +7,67 @@
 #include <math.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "display.h"
 #include "game.h"
 
 void gpio_handler(int signo){
-   if(signo == SIGIO){
-        //TODO:: for debugging purposes only
-        interrupt_triggered = 1;    
+   if(signo == SIGIO){ 
         fill_screen(6);
+        update_player_direction(&players, (char) getc(driver));
    }
 }
+
+int update_player_direction(struct player_t *players, char buttons_now){
+
+    char button_changes = (buttons_now ^ g_buttons_last);
+    
+    //Player 1 controls
+    if(button_changes & SW_1) {
+        if(players[0].dir != RIGHT) {
+            players[0].dir = LEFT;
+        }
+        g_running = 0; //TODO:: DEBUGGIN SHAIT, LEAVES MAIN WHILE
+    }
+    if(button_changes & SW_2) {
+        if(players[0].dir != DOWN) {
+            players[0].dir = UP;
+        }
+    }
+    if(button_changes & SW_3) {
+        if(players[0].dir != LEFT) {
+            players[0].dir = RIGHT;
+        }
+    }
+    if(button_changes & SW_4) {
+        if(players[0].dir != UP) {
+            players[0].dir = DOWN;
+        }
+    }
+    //Player 2 controls
+    if(button_changes & SW_5) {
+        if(players[1].dir != RIGHT) {
+            players[1].dir = LEFT;
+        }
+    }
+    if(button_changes & SW_6) {
+        if(players[1].dir != DOWN) {
+            players[1].dir = UP;
+        }
+    }
+    if(button_changes & SW_7) {
+        if(players[1].dir != LEFT) {
+            players[1].dir = RIGHT;
+        }
+    }
+    if(button_changes & SW_8) {
+        if(players[1].dir != UP) {
+            players[1].dir = DOWN;
+        }
+    }
+    g_buttons_last = buttons_now;
+} 
 
 int gamepad_init(){
 
@@ -42,15 +92,15 @@ int gamepad_init(){
 }
 
 // Initialize all grid cells to zero
-void grid_init(bool *grid, int height, int width){
+void grid_init(uint8_t **grid, int height, int width){
     for (int row = 0; row < height; row++){
         for (int col = 0; col < width; col++){
-            grid[row][col] = 0;
+            grid[col][row] = CELL_FREE;
         }
     }
 }
 
-void players_init(bool *grid, int height, int width, struct player_t *players, int n_players){
+void players_init(uint8_t **grid, int height, int width, struct player_t *players, int n_players){
     for (int player = 0; player < n_players; player++){
         players[player].pos.x = width*(player+1)/(n_players+1);
         players[player].pos.y = height/2;
@@ -59,37 +109,85 @@ void players_init(bool *grid, int height, int width, struct player_t *players, i
     }
 }
 
-int update_pos(dir_t *dir_next, position_t *head_pos){
-    switch(*dir_next){
-        case LEFT:
-            head_pos->x = head_pos->x-1;
-            break;
-        case UP:
-            head_pos->y = head_pos->y-1;
-            break;
-        case RIGHT:
-            head_pos->x = head_pos->x+1;
-            break;
-        case DOWN:
-            head_pos->y = head_pos->y+1;
-            break;
-        default:
-            return -1;
-            printf("Error: unhandled direction\n");
+int update_pos(uint8_t **grid, struct player_t *players, int n_players){
+    for(int player=0; player < n_players; player++){
+        switch(players[player].dir_next){
+            case LEFT:
+                players[player].pos.x = players[player].pos.x-1;
+                break;
+            case UP:
+                players[player].pos.y = players[player].pos.y-1;
+                break;
+            case RIGHT:
+                players[player].pos.x = players[player].pos.x+1;
+                break;
+            case DOWN:
+                players[player].pos.y = players[player].pos.y+1;
+                break;
+            default:
+                return -1;
+                printf("Error: unhandled direction\n");
+        }
+        if(detect_crash(grid, GRID_HEIGHT, GRID_WIDTH, players[player].pos){
+            //handle death
+            return 1;
+        }else{
+            grid[players[player].pos.x][players[player].pos.y] = (player+1);
+            display_fill_cell(GAME_PIXEL_SIZE, players[player].pos, (player+1));
+        }
     }
     return 0;
 }
 
+// returns 1 if move is illigal, 0 otherwise
+int detect_crash(uint8_t **grid,int grid_heigth, int grid_length, position_t pos){
+    //Check if position is inside the grid bounderies
+    if ((pos.x>grid_length-1) || (pos.x<0)){
+        //out of bounds sideways
+        return 1;
+    }
+    if ((pos.y>grid_height-1) || (pos.y<0)){
+        //out of bounds in the other direction
+        return 1;
+    }
+    
+    if(grid[pos.x][pos.y] > 0){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
 	printf("Initializing game\n");
-	interrupt_triggered = 0;        //TODO:: for debugging purposes only
+	clock_t start, end;
+	int remaining_time;
+	struct player_t players[NUM_PLAYERS];
+	
 	display_init();
-	fill_screen(13);
     gamepad_init();
+    grid_init(grid, GRID_HEIGHT, GRID_WIDTH);
+    players_init(grid, GRID_HEIGHT, GRID_WIDTH, players, NUM_PLAYERS);
+    
+    fill_screen(13);
+    g_running = 1;
+    
     while(running) {
+        start = clock();
+        
+        update_position(grid, players, NUM_PLAYERS);
+        
+        end = clock();
+        
+        remaining_time = TIME_PER_LOOP - (((end - start)/CLOCKS_PER_SEC)*1000);
+        
+        if(remaining_time > 0) {
+            usleep(remaining_time*1000);
+        }
+        
         continue;
     }
-
 	exit(EXIT_SUCCESS);
 }
--- ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.h
+++ ex3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.h
@@ -1,6 +1,24 @@
 #ifndef _GAME_H
 #define _GAME_H
 
+#define GRID_HEIGHT     SCREEN_HEIGHT/GAME_PIXEL_SIZE
+#define GRID_WIDTH      SCREEN_WIDTH/GAME_PIXEL_SIZE
+
+#define CELL_FREE       0
+#define CELL_BLOCKED    1
+
+#define TIME_PER_LOOP   100
+#define NUM_PLAYERS     2
+
+#define SW_8            0b10000000
+#define SW_7            0b01000000
+#define SW_6            0b00100000
+#define SW_5            0b00010000
+#define SW_4            0b00001000
+#define SW_3            0b00000100
+#define SW_2            0b00000010
+#define SW_1            0b00000001
+
 typedef enum{
     LEFT,
     UP,
@@ -14,15 +32,25 @@ typedef struct position_t{
     uint32_t y;
 }position_t;
 
-struct snake_t{
-    struct position_t head_pos;
-    dir_t dir_next;
-    dir_t dir_last;
+struct player_t{
+    struct position_t pos;
+    dir_t dir;
 };
+
+uint8_t grid[GRID_WIDTH][GRID_HEIGHT];
+char g_buttons_last;
+int g_running;
 
 FILE *driver;
 int interrupt_triggered;    //TODO:: for debugging purposes only
 int gamepad_init();
-int update_pos(dir_t *dir_next, position_t *head_pos);
+void grid_init(uint8_t **grid, int height, int width);
+void players_init(uint8_t **grid, 
+        int height, 
+        int width, 
+        struct player_t *players, 
+        int n_players);
+int update_pos(uint8_t **grid, struct player_t *players, int n_players);
+int detect_crash(uint8_t **grid,int grid_heigth,int grid_length, position_t pos);
 
 #endif
